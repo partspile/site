52abddef5bb1ebc511183efa9a3099a66bf3e78a
Scott Feldman <sfeldma@gmail.com>
2025-06-21 22:09:24 -0700

    refactor: move gomponents from ad.go to ui/ad.go

--- handlers/ad.go
+++ handlers/ad.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,86 +9,12 @@ import (
 	"github.com/parts-pile/site/ui"
 	"github.com/parts-pile/site/user"
 	"github.com/parts-pile/site/vehicle"
-	g "maragu.dev/gomponents"
-	hx "maragu.dev/gomponents-htmx"
-	. "maragu.dev/gomponents/html"
 )
 
 func HandleNewAd(c *fiber.Ctx) error {
 	currentUser := c.Locals("user").(*user.User)
-
 	makes := vehicle.GetMakes()
-	makeOptions := []g.Node{}
-	for _, makeName := range makes {
-		makeOptions = append(makeOptions,
-			Option(Value(makeName), g.Text(makeName)),
-		)
-	}
-
-	return render(c, ui.Page(
-		"New Ad - Parts Pile",
-		currentUser,
-		c.Path(),
-		[]g.Node{
-			ui.PageHeader("Create New Ad"),
-			Form(
-				ID("newAdForm"),
-				Class("space-y-6"),
-				EncType("multipart/form-data"),
-				ui.ValidationErrorContainer(),
-				ui.FormGroup("Make", "make",
-					Select(
-						ID("make"),
-						Name("make"),
-						Class("w-full p-2 border rounded"),
-						hx.Trigger("change"),
-						hx.Get("/api/years"),
-						hx.Target("#yearsDiv"),
-						hx.Include("this"),
-						g.Attr("onchange", "document.getElementById('modelsDiv').innerHTML = ''; document.getElementById('enginesDiv').innerHTML = '';"),
-						Option(Value(""), g.Text("Select a make")),
-						g.Group(makeOptions),
-					),
-				),
-				Div(
-					ID("yearsDiv"),
-					Class("space-y-2"),
-				),
-				Div(
-					ID("modelsDiv"),
-					Class("space-y-2"),
-				),
-				Div(
-					ID("enginesDiv"),
-					Class("space-y-2"),
-				),
-				ui.FormGroup("Description", "description",
-					Textarea(
-						ID("description"),
-						Name("description"),
-						Class("w-full p-2 border rounded"),
-						Rows("4"),
-					),
-				),
-				ui.FormGroup("Price", "price",
-					Input(
-						Type("number"),
-						ID("price"),
-						Name("price"),
-						Class("w-full p-2 border rounded"),
-						Step("0.01"),
-					),
-				),
-				ui.StyledButton("Submit", ui.ButtonPrimary,
-					Type("submit"),
-					hx.Post("/api/new-ad"),
-					hx.Encoding("multipart/form-data"),
-					hx.Target("#result"),
-				),
-				ui.ResultContainer(),
-			),
-		},
-	))
+	return render(c, ui.NewAdPage(currentUser, c.Path(), makes))
 }
 
 func HandleNewAdSubmission(c *fiber.Ctx) error {
@@ -156,33 +81,7 @@ func HandleViewAd(c *fiber.Ctx) error {
 
 	currentUser, _ := c.Locals("user").(*user.User)
 
-	actionButtons := []g.Node{
-		ui.BackToListingsButton(),
-	}
-
-	if currentUser != nil && currentUser.ID == ad.UserID {
-		editButton := ui.StyledLink("Edit Ad", fmt.Sprintf("/edit-ad/%d", ad.ID), ui.ButtonPrimary)
-		deleteButton := ui.DeleteButton(ad.ID)
-		actionButtons = append(actionButtons, editButton, deleteButton)
-	}
-
-	return render(c, ui.Page(
-		fmt.Sprintf("Ad %d - Parts Pile", ad.ID),
-		currentUser,
-		c.Path(),
-		[]g.Node{
-			Div(
-				Class("max-w-2xl mx-auto"),
-				ui.PageHeader(ad.Make),
-				ui.AdDetails(ad),
-				ui.ActionButtons(actionButtons...),
-				Div(
-					ID("result"),
-					Class("mt-4"),
-				),
-			),
-		},
-	))
+	return render(c, ui.ViewAdPage(currentUser, c.Path(), ad))
 }
 
 func HandleEditAd(c *fiber.Ctx) error {
@@ -204,145 +103,14 @@ func HandleEditAd(c *fiber.Ctx) error {
 
 	// Prepare make options
 	makes := vehicle.GetMakes()
-	makeOptions := []g.Node{}
-	for _, makeName := range makes {
-		attrs := []g.Node{Value(makeName)}
-		if makeName == ad.Make {
-			attrs = append(attrs, Selected())
-		}
-		attrs = append(attrs, g.Text(makeName))
-		makeOptions = append(makeOptions, Option(attrs...))
-	}
-
 	// Prepare year checkboxes
 	years := vehicle.GetYears(ad.Make)
-	yearCheckboxes := []g.Node{}
-	for _, year := range years {
-		isChecked := false
-		for _, adYear := range ad.Years {
-			if year == adYear {
-				isChecked = true
-				break
-			}
-		}
-		yearCheckboxes = append(yearCheckboxes,
-			ui.Checkbox("years", year, year, isChecked, false,
-				hx.Trigger("change"),
-				hx.Get("/api/models"),
-				hx.Target("#modelsDiv"),
-				hx.Include("[name='make'],[name='years']:checked"),
-			),
-		)
-	}
-
 	// Prepare model checkboxes
 	modelAvailability := vehicle.GetModelsWithAvailability(ad.Make, ad.Years)
-	modelCheckboxes := []g.Node{}
-	models := make([]string, 0, len(modelAvailability))
-	for m := range modelAvailability {
-		models = append(models, m)
-	}
-	sort.Strings(models)
-
-	for _, modelName := range models {
-		isAvailable := modelAvailability[modelName]
-		isChecked := false
-		for _, adModel := range ad.Models {
-			if modelName == adModel {
-				isChecked = true
-				break
-			}
-		}
-		modelCheckboxes = append(modelCheckboxes,
-			ui.Checkbox("models", modelName, modelName, isChecked, !isAvailable,
-				hx.Trigger("change"),
-				hx.Get("/api/engines"),
-				hx.Target("#enginesDiv"),
-				hx.Include("[name='make'],[name='years']:checked,[name='models']:checked"),
-			),
-		)
-	}
-
 	// Prepare engine checkboxes
 	engineAvailability := vehicle.GetEnginesWithAvailability(ad.Make, ad.Years, ad.Models)
-	engineCheckboxes := []g.Node{}
-	engines := make([]string, 0, len(engineAvailability))
-	for e := range engineAvailability {
-		engines = append(engines, e)
-	}
-	sort.Strings(engines)
 
-	for _, engineName := range engines {
-		isAvailable := engineAvailability[engineName]
-		isChecked := false
-		for _, adEngine := range ad.Engines {
-			if engineName == adEngine {
-				isChecked = true
-				break
-			}
-		}
-		engineCheckboxes = append(engineCheckboxes,
-			ui.Checkbox("engines", engineName, engineName, isChecked, !isAvailable),
-		)
-	}
-
-	return render(c, ui.Page(
-		"Edit Ad - Parts Pile",
-		currentUser,
-		c.Path(),
-		[]g.Node{
-			ui.PageHeader("Edit Ad"),
-			Form(
-				ID("editAdForm"),
-				Class("space-y-6"),
-				EncType("multipart/form-data"),
-				ui.ValidationErrorContainer(),
-				ui.FormGroup("Make", "make",
-					Select(
-						ID("make"),
-						Name("make"),
-						Class("w-full p-2 border rounded"),
-						hx.Trigger("change"),
-						hx.Get("/api/years"),
-						hx.Target("#yearsDiv"),
-						hx.Include("this"),
-						g.Attr("onchange", "document.getElementById('modelsDiv').innerHTML = ''; document.getElementById('enginesDiv').innerHTML = '';"),
-						Option(Value(""), g.Text("Select a make")),
-						g.Group(makeOptions),
-					),
-				),
-				ui.FormGroup("Years", "years", Div(ID("yearsDiv"), ui.GridContainer(5, yearCheckboxes...))),
-				ui.FormGroup("Models", "models", Div(ID("modelsDiv"), ui.GridContainer(5, modelCheckboxes...))),
-				ui.FormGroup("Engines", "engines", Div(ID("enginesDiv"), ui.GridContainer(5, engineCheckboxes...))),
-				ui.FormGroup("Description", "description",
-					Textarea(
-						ID("description"),
-						Name("description"),
-						Class("w-full p-2 border rounded"),
-						Rows("4"),
-						g.Text(ad.Description),
-					),
-				),
-				ui.FormGroup("Price", "price",
-					Input(
-						Type("number"),
-						ID("price"),
-						Name("price"),
-						Class("w-full p-2 border rounded"),
-						Step("0.01"),
-						Value(fmt.Sprintf("%.2f", ad.Price)),
-					),
-				),
-				ui.StyledButton("Submit", ui.ButtonPrimary,
-					Type("submit"),
-					hx.Post(fmt.Sprintf("/api/update-ad/%d", ad.ID)),
-					hx.Encoding("multipart/form-data"),
-					hx.Target("#result"),
-				),
-				ui.ResultContainer(),
-			),
-		},
-	))
+	return render(c, ui.EditAdPage(currentUser, c.Path(), ad, makes, years, modelAvailability, engineAvailability))
 }
 
 func HandleUpdateAdSubmission(c *fiber.Ctx) error {
--- ui/ad.go
+++ ui/ad.go
@@ -6,9 +6,11 @@ import (
 	"time"
 
 	g "maragu.dev/gomponents"
+	hx "maragu.dev/gomponents-htmx"
 	. "maragu.dev/gomponents/html"
 
 	"github.com/parts-pile/site/ad"
+	"github.com/parts-pile/site/user"
 )
 
 // ---- Ad Components ----
@@ -79,4 +81,250 @@ func BuildAdListNodes(ads map[int]ad.Ad, loc *time.Location) []g.Node {
 		adsList = append(adsList, AdCard(ad, loc))
 	}
 	return adsList
+}
+
+// ---- Ad Pages ----
+
+func NewAdPage(currentUser *user.User, path string, makes []string) g.Node {
+	makeOptions := []g.Node{}
+	for _, makeName := range makes {
+		makeOptions = append(makeOptions,
+			Option(Value(makeName), g.Text(makeName)),
+		)
+	}
+
+	return Page(
+		"New Ad - Parts Pile",
+		currentUser,
+		path,
+		[]g.Node{
+			PageHeader("Create New Ad"),
+			Form(
+				ID("newAdForm"),
+				Class("space-y-6"),
+				EncType("multipart/form-data"),
+				ValidationErrorContainer(),
+				FormGroup("Make", "make",
+					Select(
+						ID("make"),
+						Name("make"),
+						Class("w-full p-2 border rounded"),
+						hx.Trigger("change"),
+						hx.Get("/api/years"),
+						hx.Target("#yearsDiv"),
+						hx.Include("this"),
+						g.Attr("onchange", "document.getElementById('modelsDiv').innerHTML = ''; document.getElementById('enginesDiv').innerHTML = '';"),
+						Option(Value(""), g.Text("Select a make")),
+						g.Group(makeOptions),
+					),
+				),
+				Div(
+					ID("yearsDiv"),
+					Class("space-y-2"),
+				),
+				Div(
+					ID("modelsDiv"),
+					Class("space-y-2"),
+				),
+				Div(
+					ID("enginesDiv"),
+					Class("space-y-2"),
+				),
+				FormGroup("Description", "description",
+					Textarea(
+						ID("description"),
+						Name("description"),
+						Class("w-full p-2 border rounded"),
+						Rows("4"),
+					),
+				),
+				FormGroup("Price", "price",
+					Input(
+						Type("number"),
+						ID("price"),
+						Name("price"),
+						Class("w-full p-2 border rounded"),
+						Step("0.01"),
+					),
+				),
+				StyledButton("Submit", ButtonPrimary,
+					Type("submit"),
+					hx.Post("/api/new-ad"),
+					hx.Encoding("multipart/form-data"),
+					hx.Target("#result"),
+				),
+				ResultContainer(),
+			),
+		},
+	)
+}
+
+func ViewAdPage(currentUser *user.User, path string, ad ad.Ad) g.Node {
+	actionButtons := []g.Node{
+		BackToListingsButton(),
+	}
+
+	if currentUser != nil && currentUser.ID == ad.UserID {
+		editButton := StyledLink("Edit Ad", fmt.Sprintf("/edit-ad/%d", ad.ID), ButtonPrimary)
+		deleteButton := DeleteButton(ad.ID)
+		actionButtons = append(actionButtons, editButton, deleteButton)
+	}
+
+	return Page(
+		fmt.Sprintf("Ad %d - Parts Pile", ad.ID),
+		currentUser,
+		path,
+		[]g.Node{
+			Div(
+				Class("max-w-2xl mx-auto"),
+				PageHeader(ad.Make),
+				AdDetails(ad),
+				ActionButtons(actionButtons...),
+				Div(
+					ID("result"),
+					Class("mt-4"),
+				),
+			),
+		},
+	)
+}
+
+func EditAdPage(currentUser *user.User, path string, currentAd ad.Ad, makes []string, years []string, modelAvailability map[string]bool, engineAvailability map[string]bool) g.Node {
+	// Prepare make options
+	makeOptions := []g.Node{}
+	for _, makeName := range makes {
+		attrs := []g.Node{Value(makeName)}
+		if makeName == currentAd.Make {
+			attrs = append(attrs, Selected())
+		}
+		attrs = append(attrs, g.Text(makeName))
+		makeOptions = append(makeOptions, Option(attrs...))
+	}
+
+	// Prepare year checkboxes
+	yearCheckboxes := []g.Node{}
+	for _, year := range years {
+		isChecked := false
+		for _, adYear := range currentAd.Years {
+			if year == adYear {
+				isChecked = true
+				break
+			}
+		}
+		yearCheckboxes = append(yearCheckboxes,
+			Checkbox("years", year, year, isChecked, false,
+				hx.Trigger("change"),
+				hx.Get("/api/models"),
+				hx.Target("#modelsDiv"),
+				hx.Include("[name='make'],[name='years']:checked"),
+			),
+		)
+	}
+
+	// Prepare model checkboxes
+	modelCheckboxes := []g.Node{}
+	models := make([]string, 0, len(modelAvailability))
+	for m := range modelAvailability {
+		models = append(models, m)
+	}
+	sort.Strings(models)
+
+	for _, modelName := range models {
+		isAvailable := modelAvailability[modelName]
+		isChecked := false
+		for _, adModel := range currentAd.Models {
+			if modelName == adModel {
+				isChecked = true
+				break
+			}
+		}
+		modelCheckboxes = append(modelCheckboxes,
+			Checkbox("models", modelName, modelName, isChecked, !isAvailable,
+				hx.Trigger("change"),
+				hx.Get("/api/engines"),
+				hx.Target("#enginesDiv"),
+				hx.Include("[name='make'],[name='years']:checked,[name='models']:checked"),
+			),
+		)
+	}
+
+	// Prepare engine checkboxes
+	engineCheckboxes := []g.Node{}
+	engines := make([]string, 0, len(engineAvailability))
+	for e := range engineAvailability {
+		engines = append(engines, e)
+	}
+	sort.Strings(engines)
+
+	for _, engineName := range engines {
+		isAvailable := engineAvailability[engineName]
+		isChecked := false
+		for _, adEngine := range currentAd.Engines {
+			if engineName == adEngine {
+				isChecked = true
+				break
+			}
+		}
+		engineCheckboxes = append(engineCheckboxes,
+			Checkbox("engines", engineName, engineName, isChecked, !isAvailable),
+		)
+	}
+
+	return Page(
+		"Edit Ad - Parts Pile",
+		currentUser,
+		path,
+		[]g.Node{
+			PageHeader("Edit Ad"),
+			Form(
+				ID("editAdForm"),
+				Class("space-y-6"),
+				EncType("multipart/form-data"),
+				ValidationErrorContainer(),
+				FormGroup("Make", "make",
+					Select(
+						ID("make"),
+						Name("make"),
+						Class("w-full p-2 border rounded"),
+						hx.Trigger("change"),
+						hx.Get("/api/years"),
+						hx.Target("#yearsDiv"),
+						hx.Include("this"),
+						g.Attr("onchange", "document.getElementById('modelsDiv').innerHTML = ''; document.getElementById('enginesDiv').innerHTML = '';"),
+						Option(Value(""), g.Text("Select a make")),
+						g.Group(makeOptions),
+					),
+				),
+				FormGroup("Years", "years", Div(ID("yearsDiv"), GridContainer(5, yearCheckboxes...))),
+				FormGroup("Models", "models", Div(ID("modelsDiv"), GridContainer(5, modelCheckboxes...))),
+				FormGroup("Engines", "engines", Div(ID("enginesDiv"), GridContainer(5, engineCheckboxes...))),
+				FormGroup("Description", "description",
+					Textarea(
+						ID("description"),
+						Name("description"),
+						Class("w-full p-2 border rounded"),
+						Rows("4"),
+						g.Text(currentAd.Description),
+					),
+				),
+				FormGroup("Price", "price",
+					Input(
+						Type("number"),
+						ID("price"),
+						Name("price"),
+						Class("w-full p-2 border rounded"),
+						Step("0.01"),
+						Value(fmt.Sprintf("%.2f", currentAd.Price)),
+					),
+				),
+				StyledButton("Submit", ButtonPrimary,
+					Type("submit"),
+					hx.Post(fmt.Sprintf("/api/update-ad/%d", currentAd.ID)),
+					hx.Encoding("multipart/form-data"),
+					hx.Target("#result"),
+				),
+				ResultContainer(),
+			),
+		},
+	)
 }
